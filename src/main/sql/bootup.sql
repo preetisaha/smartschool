1c92fa04ce5de75893c90696a3ffefe961a4a4f8
Preeti Saha <preetisaha.a1@gmail.com>
2017-04-20 20:50:09 -0500

    adding sql file

---
+++ src/main/sql/bootup.sql
@@ -0,0 +1,136 @@
+CREATE SCHEMA `SmartSchool` ;
+use SmartSchool;
+
+CREATE TABLE `SmartSchool`.`Login` (
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NULL,
+  `userId` INT NOT NULL,
+  PRIMARY KEY (`email`));
+
+CREATE TABLE `SmartSchool`.`Teacher` (
+  `teacherId` INT NOT NULL AUTO_INCREMENT,
+  `teacherName` VARCHAR(45) NOT NULL,
+  `designation` VARCHAR(45) NULL,
+  PRIMARY KEY (`teacherId`));
+
+CREATE TABLE `SmartSchool`.`Student` (
+  `studentId` INT NOT NULL AUTO_INCREMENT,
+  `studentName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`studentId`));
+
+CREATE TABLE `SmartSchool`.`course` (
+  `courseId` INT NOT NULL AUTO_INCREMENT,
+  `courseName` VARCHAR(45) NOT NULL,
+  `teacherId` INT NULL,
+  `startDate` DATE NULL,
+  `endDate` DATE NULL,
+  PRIMARY KEY (`courseId`),
+  INDEX `teacherId_idx` (`teacherId` ASC),
+  CONSTRAINT `teacherId`
+    FOREIGN KEY (`teacherId`)
+    REFERENCES `SmartSchool`.`Teacher` (`teacherId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `SmartSchool`.`student_course` (
+  `studentId` INT NOT NULL AUTO_INCREMENT,
+  `student_course_id` INT NOT NULL,
+  INDEX `studentId_idx` (`studentId` ASC),
+  INDEX `student_course_id_idx` (`student_course_id` ASC),
+  CONSTRAINT `studentId`
+    FOREIGN KEY (`studentId`)
+    REFERENCES `SmartSchool`.`Student` (`studentId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `student_course_id`
+    FOREIGN KEY (`student_course_id`)
+    REFERENCES `SmartSchool`.`course` (`courseId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `SmartSchool`.`exam` (
+  `examId` INT NOT NULL AUTO_INCREMENT,
+  `examName` VARCHAR(45) NULL,
+  `startDate` DATE NULL,
+  `endDate` DATE NULL,
+  `duration` VARCHAR(45) NULL,
+  `marks` INT NULL,
+  `courseId` INT NULL,
+  PRIMARY KEY (`examId`),
+  INDEX `courseId_idx` (`courseId` ASC),
+  CONSTRAINT `courseId`
+    FOREIGN KEY (`courseId`)
+    REFERENCES `SmartSchool`.`course` (`courseId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `SmartSchool`.`question` (
+  `questionId` INT NOT NULL AUTO_INCREMENT,
+  `question` VARCHAR(45) NULL,
+  `marks` INT NULL,
+  `type` VARCHAR(45) NULL,
+  `examId` INT NOT NULL,
+  PRIMARY KEY (`questionId`),
+  INDEX `examId_idx` (`examId` ASC),
+  CONSTRAINT `examId`
+    FOREIGN KEY (`examId`)
+    REFERENCES `SmartSchool`.`exam` (`examId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `SmartSchool`.`multiple_choice` (
+  `multiple_choice_id` INT NOT NULL AUTO_INCREMENT,
+  `choice_type` VARCHAR(45) NULL,
+  `answer` VARCHAR(45) NULL,
+  `questionId` INT NOT NULL,
+  PRIMARY KEY (`multiple_choice_id`),
+  INDEX `questionId_idx` (`questionId` ASC),
+  CONSTRAINT `questionId`
+    FOREIGN KEY (`questionId`)
+    REFERENCES `SmartSchool`.`question` (`questionId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `SmartSchool`.`answer` (
+  `answerId` INT NOT NULL AUTO_INCREMENT,
+  `question_id` INT(11) NOT NULL,
+  `answer` VARCHAR(45) NULL,
+  `student_id` INT(11) NOT NULL,
+  `marks` INT NULL,
+  PRIMARY KEY (`answerId`),
+  INDEX `question_id_idx` (`question_id` ASC),
+  INDEX `student_id_idx` (`student_id` ASC),
+  CONSTRAINT `question_id`
+    FOREIGN KEY (`question_id`)
+    REFERENCES `SmartSchool`.`question` (`questionId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `student_id`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `SmartSchool`.`Student` (`studentId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `SmartSchool`.`grades` (
+  `id_student` INT(11) NOT NULL,
+  `id_exam` INT(11) NOT NULL,
+  `id_course` INT(11) NOT NULL,
+  `grade` VARCHAR(45) NULL,
+  INDEX `id_student_idx` (`id_student` ASC),
+  INDEX `id_exam_idx` (`id_exam` ASC),
+  INDEX `id_course_idx` (`id_course` ASC),
+  CONSTRAINT `id_student`
+    FOREIGN KEY (`id_student`)
+    REFERENCES `SmartSchool`.`Student` (`studentId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_exam`
+    FOREIGN KEY (`id_exam`)
+    REFERENCES `SmartSchool`.`exam` (`examId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_course`
+    FOREIGN KEY (`id_course`)
+    REFERENCES `SmartSchool`.`course` (`courseId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
